2d4d94fce481e3da3a0e3ed44ccf46da20322bc8
Joshua Evans <Joshua.Evans@sanfordhealth.org>
2023-02-01 10:05:03 -0800

    Added ViewModel to Main UI

---
+++ ESAPIX_StarterUI1/ViewModels/PlanConstraint.cs
@@ -0,0 +1,27 @@
+﻿using ESAPIX.Constraints;
+using Prism.Mvvm;
+
+namespace ESAPX_StarterUI.ViewModels
+{
+    public class PlanConstraint : BindableBase //Class to represent each row / "Constraint" to be evaluated and the result from the constraint
+    {
+        public PlanConstraint(IConstraint con)
+        {
+            this.Constraint = con;
+        }
+
+        private IConstraint _constraint; //Doing the more complex formal format here (vs. the set get) b/c programming against the user interface, which needs to know what has been changed
+        public IConstraint Constraint
+        {
+            get { return _constraint; }
+            set { SetProperty(ref _constraint, value); }
+        }
+
+        private ConstraintResult _result;
+        public ConstraintResult Result
+        {
+            get { return _result; }
+            set { SetProperty(ref _result, value); }
+        }
+    }
+}
